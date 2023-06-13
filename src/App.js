48a08fee039c6d7b1b64a00dd87cd03c9e178724
Alexandre Wilk <alex@MacBook-Air-de-Alexandre.local>
2023-06-13 14:59:13 +0200

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import SignIn from "./pages/SignIn"
+import Offers from "./pages/Offers";
+import SignUp from "./pages/SignUp"
+import Profile from "./pages/Profile"
+
 export default function App() {
   return (
-    <h1 className="bg-red-100">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+        <Route path='/forgot-password' element={<ForgotPassword/>}/>
+        <Route path='/offers' element={<Offers/>}/>
+      </Routes>
+    </Router>
   )
 }
