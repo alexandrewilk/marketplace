a4783f63b791785b5c689eb265b905553b152d2f
Paul mazeau <mazeau.paul@gmail.com>
2023-06-14 16:23:22 +0200

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,7 +20,7 @@ export default function Header() {
   }
 
   return (
-    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" className="flex items-center">
           <img src="https://coloc.fr/wp-content/uploads/2023/01/Coloc.fr_-1.png" className="h-8 mr-3" alt="Flowbite Logo"/>
@@ -29,7 +29,7 @@ export default function Header() {
         <button 
           type="button" 
           onClick={() => navigate(pageState === 'Profile' ? '/profile' : '/sign-in')}
-          className={`text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 ${routeMatchPath('/sign-in') || routeMatchPath('/profile') ? 'active' : ''}`}>
+          className={`text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 ${routeMatchPath('/sign-in') || routeMatchPath('/profile') ? 'active' : ''}`}>
           {pageState}
         </button>
           <button data-collapse-toggle="navbar-sticky" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
@@ -40,7 +40,7 @@ export default function Header() {
           </button>
         </div>
         <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:border-gray-700">
             <li className={`cursor-pointer ${routeMatchPath('/') ? 'active' : ''}`} onClick={() => navigate("/")}>
               Accueil
             </li>
