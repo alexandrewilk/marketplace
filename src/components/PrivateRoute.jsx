19c6c88284e7c78cfc6e461cba2b520e0ab8074e
Alexandre Wilk <alex@MacBook-Air-de-Alexandre.local>
2023-06-14 14:18:55 +0200

    privateroute, logout

---
+++ src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Outlet, Navigate } from 'react-router-dom'
+import {useAuthStatus} from '../hooks/useAuthStatus'
+import { Dots } from "react-activity";
+import "react-activity/dist/library.css";
+export default function PrivateRoute() {
+    const{loggedIn, loading} = useAuthStatus();
+    if(loading){
+        return(<Dots/>)
+    }
+  return loggedIn ? <Outlet/> : <Navigate to='/sign-in'/>
+}
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import {auth} from '../firebase'
+import { useNavigate } from 'react-router-dom'
 export default function Profile() {
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email
+  })
+  const navigate = useNavigate();
+  const {name, email} = formData
+  function handleLogout(e){
+    e.preventDefault()
+    auth.signOut()
+    navigate('/')
+  }
   return (
-    <div>
-      Profile
-    </div>
+    <>
+    <section>
+      <h1 className='text-center text-2xl'>Profile</h1>
+      <div className='text-center'>
+        <h2>nom :{name}</h2>
+        <h2>mail : {email}</h2>
+        <button onClick={(e) =>handleLogout(e)}>SE DECONNECTER</button>
+      </div>
+    </section>
+    </>
   )
 }
