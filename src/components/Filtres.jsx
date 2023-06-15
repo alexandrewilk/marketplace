0244fd1b0e41007e40154ce23667fd08e226981a
Paul MAZEAU <mazeau.paul@gmail.com>
2023-06-15 15:49:45 +0200

    Filtre

---
+++ src/components/Filtres.jsx
@@ -0,0 +1,34 @@
+import { Box, Stack, Select, Input, Button } from '@chakra-ui/react';
+
+export default function Filtres() {
+  return (
+    <Box bg="gray.100" p={4}>
+      <Stack
+        direction={['column', 'row']}
+        spacing={4}
+      >
+        <Select placeholder="Type de logement">
+          <option value="maison">Maison</option>
+          <option value="appartement">Appartement</option>
+          <option value="studio">Studio</option>
+        </Select>
+
+        <Select placeholder="Nombre de pièces">
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+        </Select>
+
+        <Select placeholder="Prix max">
+          <option value="500">500€</option>
+          <option value="1000">1000€</option>
+          <option value="1500">1500€</option>
+        </Select>
+
+        <Input placeholder="Localisation" />
+
+        <Button colorScheme="blue">Filtrer</Button>
+      </Stack>
+    </Box>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import AnnonceCard from '../components/AnnonceCard'
 import '../styles/home.css'
+import Filtres from '../components/Filtres'
+
 
 export default function Home() {
   return (
-    <div className="grid">
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
-      <AnnonceCard/>
+    <div className="page">
+    <Filtres/>
+      <div className="grid">
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+        <AnnonceCard/>
+      </div>
     </div>
   )
 }
