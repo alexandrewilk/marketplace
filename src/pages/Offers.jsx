a293851b3237a12b45444912a09b248de084f3c5
Paul MAZEAU <mazeau.paul@gmail.com>
2023-06-19 15:44:18 +0200

    Annonce Card

--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import AnnonceCard from '../components/AnnonceCard'
 import '../styles/offres.css'
 
 export default function Offers() {
   return (
     <div className="Page">
-      Offers! 
+      <AnnonceCard/>
+      <AnnonceCard/>
+      <AnnonceCard/>
+      <AnnonceCard/>
     </div>
   )
 }
