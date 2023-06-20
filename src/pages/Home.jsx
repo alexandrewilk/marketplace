9439a153f2b5192e2105c6780f818c8b300314a1
alexandre.wilk2000@gmail.com <alexandre.wilk2000@gmail.com>
2023-06-20 14:32:51 +0200

    search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react'
 import '../styles/home.css'
-import { Box, Heading, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { Box, Heading, Input, InputGroup, InputLeftElement, UnorderedList, ListItem } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 
+const villes = require('../assets/data/villes2.json').map((v)=>{return v.city})
 
-
+function search(input){
+  return villes.filter((v)=>{return(v.slice(0, input.length) == input)})
+}
 export default function Home() {
   const [ville, setVille] = useState('');
+  const [suggestions, setSuggestions] = useState([])
   const navigate = useNavigate();
-
-  function handleKeyDown(e){
+  function handleKeyDown(e){ //naviguer quand lutilisateur presse entré
     if(e.key=='Enter'){
       navigate(`/recherche/${ville}`)
     }
+  }
+  function handleInput(e){
+    e.preventDefault();
+    if(e.target.value==''){setSuggestions([]);return}
+    setVille(e.target.value);
+    setSuggestions([...search(e.target.value)])
   }
   return (
     <div className="page">
@@ -43,9 +52,15 @@ export default function Home() {
           type="search"
           placeholder="Entrez le nom de la ville..."
           onKeyDown={handleKeyDown}
-          onChange={(e)=>{e.preventDefault();setVille(e.target.value)}}
+          onChange={handleInput}
         />
+        <UnorderedList>
+        {suggestions.map((s)=>{return(
+          <ListItem>{s}</ListItem>
+        )})}
+        </UnorderedList>
       </InputGroup>
+     
     </Box>
     </div>
   )
