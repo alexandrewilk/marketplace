4dbbf9c40eae0a8db948e88379f6dc8e84e46be3
Paul MAZEAU <mazeau.paul@gmail.com>
2023-06-21 15:43:30 +0200

    Settings

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, Box, Container, Grid } from '@chakra-ui/react';
 import SettingsCard from '../components/SettingsCard';
 import { AiOutlineUser } from 'react-icons/ai';
+import { auth } from '../firebase';
 
 export default function Settings() {
+
+  const [formData, setFormData] = useState({
+    name: auth.currentUser ? auth.currentUser.displayName : '',
+    email: auth.currentUser ? auth.currentUser.email : ''
+  });
+  const {name, email} = formData
+
   return (
     <Container maxWidth="1200px" mt={70}>
       <Box mb={12}>
       <Text fontSize="4xl" as="b">
         Paramètres
       </Text>
-      <Text fontSize="lg">Paul Mazeau, mazeau.paul@gmail.com</Text>
+      <Text fontSize="lg">{auth.currentUser ? auth.currentUser.displayName : 'tg'}, {auth.currentUser ? auth.currentUser.email : 'tg'}</Text>
       </Box>
       <Grid
         templateColumns={{ base: 'repeat(1, 1fr)', md: 'repeat(3, 1fr)' }}
