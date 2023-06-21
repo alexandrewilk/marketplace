0320005dc5ca37bc228dc60a0e69186cbfa8432f
Paul MAZEAU <mazeau.paul@gmail.com>
2023-06-21 14:13:13 +0200

    Recherche

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import '../styles/home.css'
-import { Box, Heading, Input, InputGroup, InputLeftElement, UnorderedList, ListItem } from '@chakra-ui/react';
+import { Box, Heading, Input, InputGroup, InputLeftElement, VStack, ListItem, Container, List, ListProps } from '@chakra-ui/react';
 import { SearchIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 
@@ -9,23 +9,43 @@ const villes = require('../assets/data/villes2.json').map((v)=>{return v.city})
 function search(input){
   return villes.filter((v)=>{return(v.slice(0, input.length) == input)})
 }
+
 export default function Home() {
   const [ville, setVille] = useState('');
   const [suggestions, setSuggestions] = useState([])
+  const [activeSuggestionIndex, setActiveSuggestionIndex] = useState(0);
   const navigate = useNavigate();
-  function handleKeyDown(e){ //naviguer quand lutilisateur presse entré
-    if(e.key=='Enter'){
-      navigate(`/recherche/${ville}`)
+  
+  function handleKeyDown(e){ 
+    switch(e.key) {
+      case 'Enter': // naviguer quand l'utilisateur presse entré
+        navigate(`/recherche/${suggestions[activeSuggestionIndex]}`);
+        break;
+      case 'ArrowUp': // sélectionner la suggestion précédente
+        if (activeSuggestionIndex > 0) {
+          setActiveSuggestionIndex(activeSuggestionIndex - 1);
+        }
+        break;
+      case 'ArrowDown': // sélectionner la suggestion suivante
+        if (activeSuggestionIndex < suggestions.length - 1) {
+          setActiveSuggestionIndex(activeSuggestionIndex + 1);
+        }
+        break;
     }
   }
+
   function handleInput(e){
     e.preventDefault();
-    if(e.target.value==''){setSuggestions([]);return}
+    if(e.target.value==''){
+      setSuggestions([]);
+      setActiveSuggestionIndex(0);
+      return;
+    }
     setVille(e.target.value);
     setSuggestions([...search(e.target.value)])
   }
+
   return (
-    <div className="page">
     <Box
       bgImage="url('https://coloc.fr/wp-content/uploads/2023/04/HeroImage.webp')"
       bgSize="cover"
@@ -39,29 +59,57 @@ export default function Home() {
       padding="4rem"
       margin="20px"
       borderRadius="12px"
+      height="86vh"
     >
       <Heading as="h1" size="2xl" marginBottom="1rem">
         Trouve ta colocation <Box as="span" display="inline-block" borderBottom="2px solid">idéale</Box>
       </Heading>
-      <InputGroup maxWidth="400px" marginBottom="2rem">
-        <InputLeftElement
-          pointerEvents="none"
-          children={<SearchIcon color="gray.300" />}
-        />
-        <Input
-          type="search"
-          placeholder="Entrez le nom de la ville..."
-          onKeyDown={handleKeyDown}
-          onChange={handleInput}
-        />
-        <UnorderedList>
-        {suggestions.map((s)=>{return(
-          <ListItem>{s}</ListItem>
-        )})}
-        </UnorderedList>
-      </InputGroup>
-     
+      <Box position="relative" maxWidth="400px" marginBottom="2rem">
+        <InputGroup>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<SearchIcon color="gray.300" />}
+          />
+          <Input
+            type="search"
+            placeholder="Entrez le nom de la ville..."
+            onKeyDown={handleKeyDown}
+            onChange={handleInput}
+          />
+        </InputGroup>
+        {suggestions.length > 0 && (
+            <VStack 
+              align="start" 
+              spacing={2} 
+              width="100%" 
+              maxHeight="200px" 
+              overflowY="auto" 
+              boxShadow="md" 
+              borderRadius="md" 
+              p={2} 
+              backgroundColor="white" 
+              position="absolute"
+              mt={2} 
+              zIndex={1}
+            >
+              <List w="100%">
+                {suggestions.map((s, index) => (
+                  <ListItem 
+                    key={s} 
+                    p={2} 
+                    bg={index === activeSuggestionIndex ? "gray.500" : "gray.100"} 
+                    borderRadius="md" 
+                    width="100%"
+                    _hover={{ bg: "gray.200" }}
+                  >
+                    {s}
+                  </ListItem>
+                ))}
+              </List>
+            </VStack>
+          )}
+
+      </Box>
     </Box>
-    </div>
   )
 }
