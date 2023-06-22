6d85bcf6c50bfdf6c5f7eb85686e1b7d6d351a01
Paul MAZEAU <mazeau.paul@gmail.com>
2023-06-22 15:27:40 +0200

    Page Profil et Mes Annonces

--- src/pages/MesAnnonces.jsx
+++ src/pages/MesAnnonces.jsx
@@ -1,8 +1,13 @@
-import React, {useState} from 'react';
-import { Text, Box, Container, Grid, Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, } from '@chakra-ui/react';
-
+import React, { useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import { Dots } from 'react-activity';
+import 'react-activity/dist/library.css';
+import { HiHome } from 'react-icons/hi';
+import { Text, Container, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Button, Icon, Box } from '@chakra-ui/react';
 
 export default function MesAnnonces() {
+  const [listings, setListings] = useState([]);
+  const [loadingList, setLoadingList] = useState(false);
 
   return (
     <Container maxWidth="1200px" mt={70}>
@@ -15,7 +20,11 @@ export default function MesAnnonces() {
         </BreadcrumbItem>
       </Breadcrumb>
       <Text fontSize="4xl" as="b">Mes annonces</Text>
-      
+      <Box>
+        <Button colorScheme='red' maxW='xs' leftIcon={<Icon as={HiHome} />}>
+          <RouterLink to='/create-listing'>Mettre une annonce de colocation</RouterLink>
+        </Button>
+      </Box>
     </Container>
   );
 }
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,70 +1,80 @@
-import React, {useState, useEffect} from 'react'
-import {auth, db} from '../firebase'
-import { Link, useNavigate } from 'react-router-dom'
-import { Dots } from 'react-activity'
-import "react-activity/dist/library.css";
-import {toast} from 'react-toastify'
+import React, {useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import {toast} from 'react-toastify';
 import { updateProfile } from 'firebase/auth';
-import { doc, getDocs, orderBy, updateDoc } from 'firebase/firestore';
-import {HiOutlineHomeModern} from 'react-icons/hi2'
-import { collection, query, where } from 'firebase/firestore';
-import { Text, Box, Container, Grid, Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, } from '@chakra-ui/react';
+import { doc, getDocs, orderBy, updateDoc, collection, query, where } from 'firebase/firestore';
+import { 
+  Flex, Spacer, Text, Box, Container, Breadcrumb, BreadcrumbItem, 
+  BreadcrumbLink, Accordion, AccordionItem, AccordionButton, 
+  AccordionPanel, AccordionIcon,Button, FormControl, FormLabel, Input, Spinner 
+} 
+from '@chakra-ui/react';
+import {auth, db} from '../firebase';
 
 export default function Profile() {
-  useEffect(()=>{
-    async function getUserListing(){
-      try {
-        setLoadingList(true)
-        const q = query(collection(db, 'Listings'), where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'));
-        const querySnap = await getDocs(q);
-        let listings = []
-        querySnap.forEach((doc)=>{return(listings.push({
-          id: doc.id,
-          data: doc.data()
-        }))})
-        setListings(listings)
-      } catch (error) {
-        alert(error.message)
-      }finally{setLoadingList(false)}
-    }
-    getUserListing();
-  }, [])
   const [listings, setListings] = useState([]);
   const [loadingList, setLoadingList] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email
-  })
-  const [newName, setNewName] = useState('')
+  });
+  const [newName, setNewName] = useState('');
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(false)
-  const {name, email} = formData
-  function handleLogout(e){
-    e.preventDefault()
-    auth.signOut()
-    navigate('/')
-  }
-  function onChange(e){
-    e.preventDefault()
-    setNewName(e.target.value)
-  }
+  
+  const {name, email} = formData;
 
-  async function onSubmit(e){
-    e.preventDefault()
+  const getUserListing = async () => {
     try {
-      setLoading(true)
-      if(auth.currentUser.displayName !== newName){
-        await updateProfile(auth.currentUser, {displayName: newName})
-        const docRef = doc(db, 'Users', auth.currentUser.uid)
-        await updateDoc(docRef, {name: newName})
-        window.location.reload(true)
-      }
+      setLoadingList(true);
+      const q = query(collection(db, 'Listings'), where('userRef', '==', auth.currentUser.uid), orderBy('timestamp', 'desc'));
+      const querySnap = await getDocs(q);
+      let listings = [];
+      querySnap.forEach((doc) => {
+        listings.push({
+          id: doc.id,
+          data: doc.data()
+        });
+      });
+      setListings(listings);
     } catch (error) {
-      toast.error(error.message)
-    }finally{
-   setLoading(false)
+      alert(error.message);
+    } finally {
+      setLoadingList(false);
     }
   }
+
+  useEffect(() => {
+    getUserListing();
+  }, []);
+
+  const handleLogout = (e) => {
+    e.preventDefault();
+    auth.signOut();
+    navigate('/');
+  };
+
+  const onChange = (e) => {
+    e.preventDefault();
+    setNewName(e.target.value);
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      if(auth.currentUser.displayName !== newName) {
+        await updateProfile(auth.currentUser, {displayName: newName});
+        const docRef = doc(db, 'Users', auth.currentUser.uid);
+        await updateDoc(docRef, {name: newName});
+        window.location.reload(true);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <Container maxWidth="1200px" mt={70}>
       <Breadcrumb>
@@ -77,28 +87,53 @@ export default function Profile() {
       </Breadcrumb>
       <Text fontSize="4xl" as="b">Mes informations personnelles</Text>
 
-      <div className='text-center flex flex-col items-center'>
-        <h2>nom :{name}</h2>
-        <form onSubmit={onSubmit}>
-          <input type = 'text' id='newName' value={newName} onChange={(e)=>onChange(e)} placeholder='Nouveau nom'/>
-          {loading ? <Dots/> :<button type='submit'>Changer le nom</button>}
-        </form>
-        <h2>mail : {email}</h2>
-        <button onClick={(e) =>handleLogout(e)}>SE DECONNECTER</button>
-        <button type='submit' className='bg-blue-600 max-w-xs'>
-          <Link to='/create-listing'> 
-          <HiOutlineHomeModern/> Mettre une annonce de colocation
-          </Link>
-        </button>
-      </div>
-      <div>
-        <h1 className='text-2xl text-center'>MES ANNONCES</h1>
-        {loadingList ? <Dots/> : listings.map((l)=>{
-          return(
-            <h1  key={l.id} onClick={(e)=>{e.preventDefault();navigate(`/listings/${l.id}`)}}>{l.data.adresse}</h1>
-          )
-        })}
-      </div>
+
+  <Accordion allowToggle mt="32px">
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Nom : {name}
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+    <form onSubmit={onSubmit}>
+    <FormLabel>Nouveau nom</FormLabel>
+      <Flex alignItems="center">
+        <FormControl id="newName" flex="1">
+          <Input 
+            type="text" 
+            value={newName} 
+            onChange={(e) => onChange(e)} 
+            placeholder="Nouveau nom" 
+          />
+        </FormControl>
+        <Spacer />
+        {loading ? <Spinner /> : <Button type="submit" colorScheme="blue">Changer le nom</Button>}
+      </Flex>
+    </form>
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box as="span" flex='1' textAlign='left'>
+          Adresse Email : {email}
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
     </Container>
   )
 }
