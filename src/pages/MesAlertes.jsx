ab4ae30d39bbfbc71f6ceb8056017c2a16c925bd
Paul MAZEAU <mazeau.paul@gmail.com>
2023-06-23 11:52:52 +0200

    Page alertes

--- src/pages/MesAlertes.jsx
+++ src/pages/MesAlertes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, Box, Container, Grid, Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, } from '@chakra-ui/react';
+import { Text, Box, Container, Grid, Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator,  Table, Thead, Tbody, Tfoot, Tr, Th, Td, TableCaption, TableContainer, Button} from '@chakra-ui/react';
 
 
 export default function MesAlertes() {
@@ -14,7 +14,45 @@ export default function MesAlertes() {
       </BreadcrumbItem>
     </Breadcrumb>
     <Text fontSize="4xl" as="b">Mes alertes</Text>
-    
+
+  <TableContainer mt="20px">
+  <Table size='md'>
+    <Thead>
+      <Tr>
+        <Th>Nom</Th>
+        <Th>Logement</Th>
+        <Th >Nombre de pièces</Th>
+        <Th >Prix max.</Th>
+        <Th>Action</Th>
+      </Tr>
+    </Thead>
+    <Tbody>
+      <Tr>
+        <Td>Alerte 1</Td>
+        <Td>Apartement</Td>
+        <Td >4</Td>
+        <Td >750€</Td>
+        <Td>
+            <Button size='xs' colorScheme="red" onClick={() => console.log('prout')}>Supprimer</Button>
+        </Td>
+      </Tr>
+      <Tr>
+      <Td>Alerte 2</Td>
+        <Td>Maison</Td>
+        <Td >10</Td>
+        <Td >900€</Td>
+        <Td><Button size='xs' colorScheme="red" onClick={() => console.log('prout')}>Supprimer</Button></Td>
+      </Tr>
+      <Tr>
+      <Td>Alerte 3</Td>
+        <Td>Villa</Td>
+        <Td >14</Td>
+        <Td >450€</Td>
+        <Td><Button size='xs' colorScheme="red" onClick={() => console.log('prout')}>Supprimer</Button></Td>
+      </Tr>
+    </Tbody>
+  </Table>
+</TableContainer>
   </Container>
   )
 }
