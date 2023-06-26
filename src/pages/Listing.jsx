09f03aeee9c9cf35422bd5160db9a72235632d46
alexandre.wilk2000@gmail.com <alexandre.wilk2000@gmail.com>
2023-06-26 14:27:46 +0200

    remove google maps

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,17 +3,12 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { db } from '../firebase';
 import { Dots } from 'react-activity';
-import {useJsApiLoader, GoogleMap, MarkerF} from '@react-google-maps/api'
+
 
 export default function Listing() {
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
-    const libraries = ['places']
-    const {isLoaded} = useJsApiLoader({
-        googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
-        libraries: libraries
-    })
     useEffect(()=>{
         async function fetchListing(){
             try{
@@ -37,20 +32,10 @@ export default function Listing() {
     function renderContent(){
         if(loading){return <Dots/>}
         if(!listing){return <h1>CETTE ANNONCE NEXISTE PLUS</h1>}
-        if(!isLoaded){return <Dots/>}
         const center = listing.geolocation
         return(
             <div className='h-1/2 w-1/2'>
             <h1>{listing.adresse}</h1>
-            <GoogleMap center={center} zoom={15} mapContainerStyle={containerStyle}
-            options={{
-                zoomControl: false,
-                streetViewControl: false,
-                mapTypeControl: false,
-                fullscreenControl: false
-            }}>
-                <MarkerF position={center}/>
-            </GoogleMap>
             </div>
         )
     }
