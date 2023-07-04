8da409edaf48e11665827ac28bf1a876ada55c7a
Paul MAZEAU <mazeau.paul@gmail.com>
2023-07-04 15:17:48 +0200

    Listing

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,9 +3,17 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
 import { db } from '../firebase';
 import { Dots } from 'react-activity';
-
+import { Box, Container, Grid, Image, Heading, useBreakpointValue, Flex} from "@chakra-ui/react";
+import SignInImage from '../assets/images/SignIn.jpg';
 
 export default function Listing() {
+  const gridTemplateColumnsThree = useBreakpointValue({ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" });
+  const gridTemplateRow = useBreakpointValue({ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" });
+  const gridTemplateColumnsSecondary = useBreakpointValue({ base: "1fr", md: "2fr 1fr" });
+  const gridTemplateColumns = useBreakpointValue({ base: "2fr 1fr", md: "repeat(2, 1fr)" });
+  const imageMaxHeight = useBreakpointValue({ base: "300px", md: "auto" });
+  const displayImageFour = useBreakpointValue({ base: "none", md: "block" });
+
     const params = useParams();
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -39,13 +47,30 @@ export default function Listing() {
             </div>
         )
     }
-  return (
-    <div className='flex flex-col'>
-      <h1 className='text-3xl'> OFFRE</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      <h1>TEST</h1>
-      {renderContent()}
-    </div>
-  )
+    return (
+      <Box maxW="1200px" marginX="auto" mt={6}>
+        <Grid templateColumns={gridTemplateColumns} gap={3} >
+          <Box w="100%" h="auto">
+            <Image src={SignInImage} alt="Description de l'image 1" boxSize="100%" maxH={imageMaxHeight} objectFit="cover" borderRadius="12px"/> 
+          </Box>
+          <Box w="100%" h="auto">
+            <Grid templateColumns={gridTemplateColumnsThree} templateRows={gridTemplateRow} gap={3}>
+              <Image src={SignInImage} alt="Description de l'image 2" boxSize="100%" maxH={imageMaxHeight} objectFit="cover" borderRadius="12px"/>
+              <Image src={SignInImage} alt="Description de l'image 3" boxSize="100%" maxH={imageMaxHeight} objectFit="cover" borderRadius="12px"/>
+              <Image src={SignInImage} alt="Description de l'image 4" boxSize="100%" maxH={imageMaxHeight} objectFit="cover" borderRadius="12px"/>
+              <Image src={SignInImage} alt="Description de l'image 5" boxSize="100%" maxH={imageMaxHeight} objectFit="cover" borderRadius="12px" display={displayImageFour}/>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid templateColumns={gridTemplateColumnsSecondary} gap={6} marginTop={6}>
+          <Box w="100%" h="100vh" borderWidth="1px" boxShadow='base' borderColor="gray.200" borderRadius="12px">
+            <Heading as='h1' size='xl' noOfLines={1}> Appartement teh les brid</Heading> 
+          </Box>
+          <Box w="100%" h="300" bg="white" borderWidth="1px" borderColor="gray.200" boxShadow='md' borderRadius="12px" position="sticky" top="0"/>
+        </Grid>
+        <Box backgroundColor="red" h="100vh">
+        </Box>
+      </Box>
+    );
+    
 }
