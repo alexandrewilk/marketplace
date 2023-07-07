02ff0d0c6ab151422a5eba3c958e2411ed24b3c4
Paul MAZEAU <mazeau.paul@gmail.com>
2023-07-07 15:10:02 +0200

    Annonce Liker

--- src/components/AnnonceCardPageMesLikes.jsx
+++ src/components/AnnonceCardPageMesLikes.jsx
@@ -1,15 +1,9 @@
 import { Box, Image, Flex, Text, IconButton } from "@chakra-ui/react";
 import CustomBadge from './CustomBadge';
 import IconBadge from './IconBadge';
-import { FaHouseUser, FaRegHeart, FaBed, FaBath } from "react-icons/fa";
-import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai'
-import { LikesContext } from "../context/LikesContext";
-import { useContext } from "react";
-import { arrayRemove, arrayUnion, doc, updateDoc } from "firebase/firestore";
-import { db, auth } from "../firebase";
+import { FaHouseUser, FaBed, FaBath } from "react-icons/fa";
+import { AiFillHeart } from 'react-icons/ai'
 import { useNavigate } from "react-router-dom";
-
-
 
 export default function AnnonceCard({ data, id }) {
   const { imgUrls, type, loyer, nbPieces } = data;
@@ -17,7 +11,6 @@ export default function AnnonceCard({ data, id }) {
   const badgeProperties = [
     { icon: FaBed, text: "5 chambres" },
     { icon: FaHouseUser, text: "300m²" },
-    { icon: FaBath, text: "2 salle de bain" }
   ];
 
 
@@ -42,44 +35,51 @@ export default function AnnonceCard({ data, id }) {
 //     }
 //   }
 
-  return (
-    <Box
-      borderWidth="1px"
-      bg="white"
-      borderRadius="16px"
-      overflow="hidden"
-      boxShadow="base"
-      height="200px"
-      transition="box-shadow 0.2s"
-      _hover={{ boxShadow: "md" }}
-      mb={4}
-    >
-      <Flex h="100%">
-        <Box w="35%" onClick={(e)=>{e.preventDefault();navigate(`/listings/${id}`)}}>
-          <Image src={imgUrls ? imgUrls[0] : "https://bit.ly/2k1H1t6"} alt="Appartement" objectFit="cover" w="100%" h="100%" />
+return (
+  <Box
+    borderWidth="1px"
+    bg="white"
+    borderRadius="16px"
+    overflow="hidden"
+    boxShadow="base"
+    transition="box-shadow 0.2s"
+    _hover={{ boxShadow: "md" }}
+    mb={4}
+    maxW="240px"
+    minH="280px"
+  >
+    <Flex direction={{ base: "column", md: "column" }} h="100%">
+      <Box position="relative" w="100%" onClick={(e)=>{e.preventDefault();navigate(`/listings/${id}`)}}>
+        <Image src={imgUrls ? imgUrls[0] : "https://bit.ly/2k1t6"} alt="Appartement" objectFit="cover" w="95%" h="95%" m="2.5%" borderRadius="8px"/>
+        <Box onClick={(e)=>{e.stopPropagation();}}>
+          <IconButton 
+            position="absolute" 
+            top={3} 
+            right={3}
+            icon={<AiFillHeart />}
+            isRound 
+            onClick={(e)=>{e.preventDefault()}}
+          />
         </Box>
-        <Flex width="65%" flexDirection="column" justifyContent="space-between" padding={4} h="100%">
-          <Flex justifyContent="flex-end" >
-            <IconButton icon={<AiFillHeart />  } isRound onClick={(e)=>{e.preventDefault()}}/>
-          </Flex>
-          <Flex flexDirection="column" justifyContent="space-between" h="100%" spacing={2} onClick={(e)=>{e.preventDefault();navigate(`/listings/${id}`)}}>
-            <Text fontSize="xl" fontWeight="semibold">
-              {type}, loyer de {loyer}€, {nbPieces} pièces
-            </Text>
-            <Flex>
-              {badgeProperties.map((badge, index) => (
-                <IconBadge key={index} icon={badge.icon} text={badge.text} marginRight="4px" />
-              ))}
-            </Flex>
-            <Flex justifyContent="space-between" alignItems="flex-end">
-              <CustomBadge text="SeLoger" />
-              <Text fontSize="xl" color="blue.500">
-                {loyer}€/mois
-              </Text>
-            </Flex>
+      </Box>
+
+      <Flex direction="column" padding={2} h="100%">
+        <Flex flexDirection="column" justifyContent="space-around" h="100%" onClick={(e)=>{e.preventDefault();navigate(`/listings/${id}`)}}>
+          <CustomBadge text="SeLoger"/>
+          <Text fontSize="lg" color="#172ACE" as='b'>
+            {loyer}€
+          </Text>
+          <Text fontSize="md" fontWeight="semibold" whiteSpace="nowrap" overflow="hidden" textOverflow="ellipsis">
+            {type}, loyer de {loyer}€, {nbPieces} pièces
+          </Text>
+          <Flex>
+            {badgeProperties.map((badge, index) => (
+              <IconBadge key={index} icon={badge.icon} text={badge.text} marginRight="4px" />
+            ))}
           </Flex>
         </Flex>
       </Flex>
-    </Box>
-  );
+    </Flex>
+  </Box>
+);
 }
