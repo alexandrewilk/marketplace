d07e504ec40ddeef13acf17b9b1f1ae9379613f5
Paul MAZEAU <mazeau.paul@gmail.com>
2023-07-12 12:15:41 +0200

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,7 +73,7 @@ export default function Home() {
                   <Image src={Entourement} alt="Description of SVG" position="absolute" top={0} left={0} width="100%" height="100%"/>
               </Box>
           </Heading>
-      </Box>
+        </Box>
 
         <Box position="relative" maxWidth="400px" marginBottom="2rem">
           <InputGroup>
